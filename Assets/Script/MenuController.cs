7b176bab87d231fd4935f2355d7718ddc113aa00
Blizary <rosajunior05@gmail.com>
2020-03-03 14:41:46 +0000

    Main menu

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Duck"))
+        {
+            PlayGame();
+        }
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
